789f752ba1aadded5c3ebaae4956598cfdf7807c
aishaortiz <developer.aishaortiz@gmail.com>
2019-09-12 13:23:24 -0700

    added boiler plates for questions | wrapped html content

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,30 @@
-console.log('Success');
+$('#start').on('click', function() {
+  console.log('Tada Trivia');
+  // $('#sub-container').remove();
+});
+
+var questions = [{
+question: 'Which actress said, "Fasten your seatbelts. Its going to be a bumpy night," in All About Eve?' ,
+answers:['guess 0', 'guess 1', 'guess 2', 'Bette Davis (as Margo Channing.)'],
+correctAnswer: '' 
+}];
+var questions = [{
+question: 'Which actress said, "Fasten your seatbelts. Its going to be a bumpy night," in All About Eve?' ,
+answers:['guess 0', 'guess 1', 'guess 2', 'Bette Davis (as Margo Channing.)'],
+correctAnswer: ''
+}];
+var questions = [{
+question: 'Which actress said, "Fasten your seatbelts. Its going to be a bumpy night," in All About Eve?' ,
+answers:['guess 0', 'guess 1', 'guess 2', 'Bette Davis (as Margo Channing.)'],
+correctAnswer: ''
+}];
+var questions = [{
+question: 'Which actress said, "Fasten your seatbelts. Its going to be a bumpy night," in All About Eve?' ,
+answers:['guess 0', 'guess 1', 'guess 2', 'Bette Davis (as Margo Channing.)'],
+correctAnswer: ''
+}];
+var questions = [{
+question: 'Which actress said, "Fasten your seatbelts. Its going to be a bumpy night," in All About Eve?' ,
+answers:['guess 0', 'guess 1', 'guess 2', 'Bette Davis (as Margo Channing.)'],
+correctAnswer: ''
+}];
