e604ecb83ba2f967ca0cb30b98007b53bb183129
aishaortiz <developer.aishaortiz@gmail.com>
2019-09-19 21:36:33 -0700

    fixed bug in questions rendering

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -10,27 +10,18 @@ $('#start').on('click', function() {
 });
 
 var questions = [{
-question: 'Which actress said, "Fasten your seatbelts. Its going to be a bumpy night," in All About Eve?' ,
-answers:['guess 0', 'guess 1', 'guess 2', 'Bette Davis (as Margo Channing.)'],
-correctAnswer: '' 
-}];
-var questions = [{
-question: 'Which actress said, "Fasten your seatbelts. Its going to be a bumpy night," in All About Eve?' ,
-answers:['guess 0', 'guess 1', 'guess 2', 'Bette Davis (as Margo Channing.)'],
-correctAnswer: ''
-}];
-var questions = [{
-question: 'Which actress said, "Fasten your seatbelts. Its going to be a bumpy night," in All About Eve?' ,
-answers:['guess 0', 'guess 1', 'guess 2', 'Bette Davis (as Margo Channing.)'],
-correctAnswer: ''
-}];
-var questions = [{
-question: 'Which actress said, "Fasten your seatbelts. Its going to be a bumpy night," in All About Eve?' ,
-answers:['guess 0', 'guess 1', 'guess 2', 'Bette Davis (as Margo Channing.)'],
-correctAnswer: ''
-}];
-var questions = [{
-question: 'Which actress said, "Fasten your seatbelts. Its going to be a bumpy night," in All About Eve?' ,
-answers:['guess 0', 'guess 1', 'guess 2', 'Bette Davis (as Margo Channing.)'],
-correctAnswer: ''
-}];
+    question: 'Which actress said, "Fasten your seatbelts. Its going to be a bumpy night," in All About Eve?' ,
+    answers:['guess 0', 'guess 1', 'guess 2', 'Bette Davis (as Margo Channing.)'],
+    correctAnswer: '',
+},
+{
+    question: 'Which actress said, "Fasten your seatbelts. Its going to be a bumpy night," in All About Eve?',
+    answers: ['guess 0', 'guess 1', 'guess 2', 'Bette Davis (as Margo Channing.)'],
+    correctAnswer: '',
+  },
+{
+    question: 'Which actress said, "Fasten your seatbelts. Its going to be a bumpy night," in All About Eve?',
+    answers: ['guess 0', 'guess 1', 'guess 2', 'Bette Davis (as Margo Channing.)'],
+    correctAnswer: '',
+  }];
+
